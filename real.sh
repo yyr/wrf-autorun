06afb66e19b41407743c0879ee4a2b7d1987953f
Yakkala Yagnesh Raghava <yagnesh@live.com>
2011-05-10 22:56:26 +0900

    new: real.sh, wrf-job.sh

---
+++ real.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+#
+# Copyright (C) Yagnesh Raghava Yakkala. www.yagnesh.org
+#    File: real.sh
+#  Author: Yagnesh Raghava Yakkala <yagnesh@live.com>
+# Created: Tuesday, May 10 2011
+# Licence: GPL v3 or later. You should get a copy from <http://www.gnu.org/licenses/gpl.html>
+#
+
+# Description: 
+if [ ! DirNames.env ]; then
+    echo "No ENV file"
+    echo "trying to contine..."
+else
+    . DirNames.env
+fi
+
+ln -sf $met_files_dir/met_em* .
+echo `pwd`
+
+#PBS -q short
+#PBS -l ncpus=1
+#PBS -N wrf_real.exe
+#PBS -o log.real
+#PBS -j oe
+
+# one proc is good for now
+echo Running real.sh
+mpirun -np 1 dplace -s1 ./real.exe 2>&1 | tee log.real 
+
+# real.sh ends here
---
+++ wrf-job.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+#
+# Copyright (C) Yagnesh Raghava Yakkala. www.yagnesh.org
+#    File: wrf-job.sh
+#  Author: Yagnesh Raghava Yakkala <yagnesh@live.com>
+# Created: Tuesday, May 10 2011
+# Licence: GPL v3 or later. You should get a copy from <http://www.gnu.org/licenses/gpl.html>
+#
+
+# Description:
+function usage() {		# tell the usage
+        echo USAGE: "$1 <cpus> <short/mid/long> "
+}
+
+
+MINARGS=2
+E_ARGCOUNT=65
+
+if [ $# -lt $MINARGS ]
+then
+    echo "${#} arguments."
+    usage $0
+    exit $E_ARGCOUNT;
+fi
+
+# make sure env file is there
+if [ ! DirNames.env ]; then
+    echo "No ENV file"
+    exit 24
+else
+    . DirNames.env
+fi
+
+# put some default cpus
+cpus=$1
+job_type=$2
+# echo cpus: $cpus
+
+echo '#!/bin/bash' > wrf.sh
+chmod +x wrf.sh
+echo "#PBS -q $job_type" >> wrf.sh
+echo '#PBS -l ncpus='$cpus >> wrf.sh
+echo "#PBS -N $run_name" >> wrf.sh
+echo "#PBS -o log.wrf" >> wrf.sh
+echo '#PBS -j oe' >> wrf.sh
+echo '' >> wrf.sh
+echo "cd $wrf_run_dir" >> wrf.sh
+echo '' >> wrf.sh
+
+echo "No of Cpus: $cps" >> wrf.sh
+echo "mpirun -np $cpus dplace -s1 ./wrf.exe " >> wrf.sh
+
+# qsub wrf.sh
+
+# wrf-job.sh ends here
