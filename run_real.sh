a93ff154e39477a8e90e36e5f8bfb2d1fc1c3233
Yakkala Yagnesh Raghava <yagnesh@live.com>
2011-06-21 00:22:11 +0900

    real.sh, fix

--- run_real.sh
+++ run_real.sh
@@ -4,12 +4,12 @@
 
 #PBS -q short
 #PBS -l ncpus=1
-#PBS -N wrf_real.exe
+#PBS -N real_real.exe
 #PBS -o log.real
 #PBS -j oe
 
-echo $WRF_run_dir
-cd  $WRF_run_dir
+echo $wrf_run_dir
+cd  $wrf_run_dir
 
 # one proc is good for now
 mpirun -np 1 dplace -s1 ./real.exe  > real.`date "+%y%m%d%H%M"`.log
