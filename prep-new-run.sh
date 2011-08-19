624ccd91e460055adf51c0a0a7bee4ed3ef68fd1
Yakkala Yagnesh Raghava <yagnesh@live.com>
2011-08-20 00:12:17 +0900

    touched files

---
+++ prep-new-run.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#
+# Copyright (C) Yagnesh Raghava Yakkala. www.yagnesh.org
+#    File: prep-new-run.sh
+#  Author: Yagnesh Raghava Yakkala <yagnesh@live.com>
+# Created: Friday, June 17 2011
+# Licence: GPL v3 or later. You should get a copy from <http://www.gnu.org/licenses/gpl.html>
+#
+
+# Description: 
+# create/copy new directories for WPS and WRF with a given name
+
+
+# prep-new-run.sh ends here
