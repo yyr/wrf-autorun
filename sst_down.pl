bec3cedae6c9d195026bd9c94cf901f33c5c37d0
Yakkala Yagnesh Raghava <yagnesh@live.com>
2011-09-16 10:29:19 +0900

    * sst_down.pl: hmm.. working now for downloads with in a month (same   as fnl data downloading)

--- sst_down.pl
+++ sst_down.pl
@@ -13,17 +13,36 @@
 use strict;
 use warnings;
 
+my $stime = 20081223;
+my $etime = 20081225;
+
+# ------- No need to change below -------
+my(@filelist);
+my $stime_prefix = substr $stime , 0, 6;
+my $etime_prefix = substr $etime , 0, 6;
 my $url_prefix = "ftp://polar.ncep.noaa.gov/pub/history/sst/rtg_sst_grb_0.5.";
 
-my @year;
-my @month= ('01','02','03','04','05','06','07','08','09','10','11','12');
+if ( $stime_prefix != $etime_prefix ) {
+  print ("script works only when the month is same\n");
+  exit 24;
+}
+
+my $sday = substr $stime , 6;
+my $eday = substr $etime , 6;
+
+
+foreach my $d ( $sday .. $eday ) {
+    $filelist[++$#filelist] = "$url_prefix" . "$stime_prefix" . "$d";
+}
+
+# print(@filelist, "\n");
 
 # --------------------------------
 sub wget_down {
   my ($url) = @_;
   my $er_log_file = "log.error.$$";
   print "started downloading.. $url\n";
-  my $er_code = system("wget", "--timeout=90","$url");
+  my $er_code = system("wget", "-N", "--timeout=90","$url");
   if ($er_code != 0 ) {
     open(my $fh_e, ">>",$er_log_file) or die "cant open logfile $!";
     print $fh_e  "$url";        # put in log for later downloading
@@ -34,17 +53,8 @@ sub wget_down {
 
 
 # body ---------------
-foreach my $y (@year) {
-  foreach my $m (@month) {
-    foreach my $d (1 .. Days_in_Month($y,$m)) {
-      if ($d < 10) {
-        $d="0$d";
-      }
-      foreach my $t ('00','06','12','18') {
-        wget_down("$url_head/$y/$y$m/$y$m$d/CFSR_$y$m$d$t.grb2");
-      }
-    }
-  }
+foreach my $file (@filelist) {
+        wget_down($file);
 }
 
 # sst_down.pl ends here
