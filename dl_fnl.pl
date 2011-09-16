3051d049655405a8eefe93e0a4ea64abd2823bbb
Yakkala Yagnesh Raghava <yagnesh@live.com>
2011-09-16 10:05:16 +0900

    * dl_fnl.pl: hmm.. first stab into automate this downloading

--- dl_fnl.pl
+++ dl_fnl.pl
@@ -1,55 +1,74 @@
-#!/usr/bin/perl -w
+#!/usr/bin/env perl
+#
+# Copyright (C) Yagnesh Raghava Yakkala. www.yagnesh.org
+#    File: dl_fnl.pl
+#  Author: Yagnesh Raghava Yakkala <yagnesh@live.com>
+# Created: Saturday, April 30 2011
+# Licence: GPL v3 or later. <http://www.gnu.org/licenses/gpl.html>
+#
 
+# Commentary
+# This is originally download from ucar site.
+# I just modified to put intial time and end time
+#
+# PROBLEMS
+# right now it works only if start and end times are in the same month
+
+# Code starts here
+# ***** CHANGES NEEDED HERE *********
 use strict;
-use warnings;
-use YAML::XS;
+my $stime = 20081226;
+my $etime = 20081228;
+my $email = 'yagneshraghava@gmail.com'; # your registered email ID
 
-# sub get_pass {
-#   my $pass_file = "~/git/wrf-autorun/passwords.yml";
-#   my $passwdref = YAML::XS::LoadFile($pass_file);
-#   print "passwd of gmail: " , $passwdref->{'fnl'},"\n";
-#}
 
-my ($syscmd, $vn, $opt, $i, @filelist);
-my @hours=('00','06','12','18');
-my @months;
+# ------- No need to change below -------
+my(@filelist);
+my $stime_prefix = substr $stime , 0, 6;
+my $etime_prefix = substr $etime , 0, 6;
+my $url_prefix = "grib1/2008/2008.12/fnl_",;
 
-# my $pswd = 'mypasswd' ; #(@ARGV ? $ARGV[0] : $ENV{RDAPSWD});
+if ( $stime_prefix != $etime_prefix ) {
+  print ("script works only when the month is same\n");
+  exit 24;
+}
 
+my $sday = substr $stime , 6;
+my $eday = substr $etime , 6;
+
+
+foreach my $d ( $sday .. $eday ) {
+  foreach my $h ('00','06','12','18') {
+    @filelist[++$#filelist] = "$url_prefix"."$stime_prefix" . "$d" . "_" . "$h" . "_00_c";
+  }
+}
+
+# --------------------------------------------
+my ($syscmd, $vn, $opt, $i);
+my $pswd = (@ARGV ? $ARGV[0] : $ENV{RDAPSWD});
 if (!$pswd) {
   print "\n Usage: $0 YourPassword\n\n";
   exit 1;
 }
-
 open VN, "wget -V |" or die 'cannot find wget';
 $vn = (<VN> =~ /^GNU Wget (\d+)\.(\d+)/) ? (100 * $1 + $2) : 109;
 close(VN);
-
 $syscmd = ($vn > 109 ? 'wget --no-check-certificate' : 'wget');
 $syscmd .= ' -O /dev/null --save-cookies auth.dss_ucar_edu --post-data' .
-  "='email=yagneshraghava\@gmail.com&passwd=$pswd&action=login' " .
+ "='email=yagneshraghava\@gmail.com&passwd=$pswd&action=login' " .
   'https://dss.ucar.edu/cgi-bin/login';
-
 system($syscmd);
-
 $opt = 'wget -N';
 $opt .= ' --no-check-certificate' if($vn > 109);
 $opt .= ' --load-cookies auth.dss_ucar_edu ' .
   'http://dss.ucar.edu/dsszone/ds083.2/';
-
-
-# "grib1/2006/2006.01/fnl_20060127_18_00",
-@filelist = (
-            );
 
 for ($i = 0; $i < @filelist; $i++) {
   $syscmd = $opt . $filelist[$i];
   print "$syscmd...\n";
   system($syscmd);
 }
-
 system('rm -f auth.dss_ucar_edu');
 exit 0;
 
-foreach ( ) {
-}
+# dl_fnl.pl ends here
