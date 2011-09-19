767d24d8c5c8476a20fcb97cf5af4d55d5828089
Yakkala Yagnesh Raghava <yagnesh@live.com>
2011-09-19 23:18:03 +0900

    * wps_clean.sh: few more things to clean

--- wps_clean.sh
+++ wps_clean.sh
@@ -7,11 +7,14 @@
 # Licence: GPL v3 or later. You should get a copy from <http://www.gnu.org/licenses/gpl.html>
 #
 
-# Description: 
+# Description:
 
-rm -rf met_em.*
-rm -rf FILE*
-rm -rf SST*
-rm -rf geo_em*
+rm -f met_em.*
+rm -f FILE*
+rm -f SST*
+rm -f geo_em*
+rm -f PFILE*
+rm -f namelist.wps.1*
+rm -f GRIBFILE.*
 
 # wps_clean.sh ends here
