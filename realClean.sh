6a3172b87260a97e573745dd1f2f14501da0289c
Yakkala Yagnesh Raghava <yagnesh@live.com>
2011-09-19 23:28:01 +0900

    * realClean.sh: cleaning stuff

---
+++ realClean.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+#
+# Copyright (C) Yagnesh Raghava Yakkala. http://yagnesh.org
+#    File: realClean.sh
+#  Author: Yagnesh Raghava Yakkala <yagnesh@NOSPAM.live.com>
+# Created: Monday, September 19 2011
+# Licence: GPL v3 or later.  <http://www.gnu.org/licenses/gpl.html>
+#
+
+# Description:
+## clean the products/garbage from real.sh
+
+rm -f met_em.*
+rm -f real.log
+rm -f wrfinput_*
+rm -f wrfbdy_d*
+
+# realClean.sh ends here
