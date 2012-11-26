4c602c3d61393a7c1c39ba04bc9e3c74fcc25ec8
Yakkala Yagnesh Raghava <hi@yagnesh.org>
2012-11-27 00:18:07 +0900

    * time-stats.sh: fix

--- time-stats.sh
+++ time-stats.sh
@@ -3,6 +3,6 @@
 # Description:
 # http://www.mmm.ucar.edu/wrf/WG2/bench/
 
-grep 'Timing for main' rsl.error.0000  | awk '{print $9}' | awk -f $runwrf/stats.awk
+grep 'Timing for main' rsl.error.0000  | awk '{print $9}' | awk -f $(cd `dirname $BASH_SOURCE`; pwd)/stats.awk
 
 # wrf_bench.sh ends here
