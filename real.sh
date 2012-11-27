6f880d24a576502d71596c090a4fdae236e44ae4
Yakkala Yagnesh Raghava <hi@yagnesh.org>
2012-11-27 18:59:14 +0900

    * real.sh: del junk

--- real.sh
+++ real.sh
@@ -19,15 +19,10 @@ fi
 
 #ln -s "$met_files_dir/met_em*" .
 echo `pwd`
-
-#PBS -q short
-#PBS -l ncpus=1
-#PBS -N wrf_real.exe
-#PBS -o log.real
-#PBS -j oe
+nocpu=1
 
 # one proc is good for now
 echo "Running real.sh"
-mpirun -np 1 dplace -s1 ./real.exe  2>&1 | tee log.real
+mpirun -np $nocpu dplace -s1 ./real.exe  2>&1 | tee log.real
 
 # real.sh ends here
