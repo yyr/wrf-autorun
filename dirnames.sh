49a812b25ea72dd514b4113b8f5249810cfa2625
Yakkala Yagnesh Raghava <hi@yagnesh.org>
2012-11-27 16:04:59 +0900

    * dirnames.sh: new template

---
+++ dirnames.sh
@@ -0,0 +1,14 @@
+# template
+export run_name=weak1
+export run_dir_prefix=/home/yagnesh/wrf/run/
+export tbls_dir=$run_dir_prefix/tbls
+
+export wrf_run_dir=$run_dir_prefix/$run_name
+export met_files_dir=$wrf_run_dir
+
+# DATA directory
+export fnl_dir=/home/yagnesh/DATA/FNL/weak1
+export sst_dir=/home/yagnesh/DATA/SST/weak1
+
+# domain dir
+export opt_output_from_geogrid_path=$wrf_run_dir
