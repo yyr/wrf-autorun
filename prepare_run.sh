6bd33a82f7bf5f11c18bd2f781a888c31eda123b
Yakkala Yagnesh Raghava <hi@yagnesh.org>
2012-12-30 08:23:34 +0900

    * prepare_run.sh: keep relative folder names. do more work.

--- prepare_run.sh
+++ prepare_run.sh
@@ -16,12 +16,13 @@ then
 fi
 
 export SCRIPTS_DIR=$(cd `dirname $BASH_SOURCE`; pwd)
-
 export run_name=$1
 
 # template
-export run_dir_prefix=$(pwd)
+export wrf_build_dir=/home/yagnesh/wrf/intel/WRFV3
 export data_dir_prefix=/home/yagnesh/DATA
+
+export run_dir_prefix=$(pwd)
 export wrf_run_dir=$run_dir_prefix/$run_name
 
 cd $run_dir_prefix
@@ -32,18 +33,22 @@ cd $run_name
 cat  <<EOF > dirnames.sh
 export run_name=$run_name
 export run_dir_prefix=$run_dir_prefix
-export tbls_dir=$run_dir_prefix/tbls
-export wrf_bin_dir=$run_dir_prefix/bin
+export tbls_dir=\$run_dir_prefix/tbls
+export wrf_bin_dir=\$run_dir_prefix/bin
 
-export wrf_run_dir=$run_dir_prefix/$run_name
-export met_files_dir=$wrf_run_dir
+export wrf_run_dir=\$run_dir_prefix/\$run_name
+export met_files_dir=\$wrf_run_dir
 
 export data_dir_prefix=$data_dir_prefix
-export fnl_dir=$data_dir_prefix/FNL/$run_name
-export sst_dir=$data_dir_prefix/SST/$run_name
+export fnl_dir=\$data_dir_prefix/FNL/\$run_name
+export sst_dir=\$data_dir_prefix/SST/\$run_name
 
-export opt_output_from_geogrid_path=$wrf_run_dir
+export opt_output_from_geogrid_path=\$wrf_run_dir
 EOF
+
+ln -sf $wrf_build_dir/run/*TBL* .
+ln -sf $wrf_build_dir/run/*DATA* .
+cp $run_dir_prefix/nl/namelist* .
 
 echo "prepared $run_dir_prefix/$run_name for wrf run named \"$run_name\""
 # prepare_run.sh ends here
