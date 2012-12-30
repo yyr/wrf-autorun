8a1ce991e29bd7ee3133e43c724236d0d04d28d6
Yakkala Yagnesh Raghava <hi@yagnesh.org>
2012-12-30 11:16:26 +0900

    * time-stats.sh: limit domain 3 only to increase accuracy of stats.

--- time-stats.sh
+++ time-stats.sh
@@ -3,6 +3,9 @@
 # Description:
 # http://www.mmm.ucar.edu/wrf/WG2/bench/
 
-grep 'Timing for main' rsl.error.0000  | awk '{print $9}' | awk -f $(cd `dirname $BASH_SOURCE`; pwd)/stats.awk
+grep 'Timing for main' rsl.error.0000  | \
+    grep " 3:" | \
+    awk '{print $9}' | \
+    awk -f $(cd `dirname $BASH_SOURCE`; pwd)/stats.awk
 
 # wrf_bench.sh ends here
