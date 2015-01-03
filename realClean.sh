57ffd8b349c348188ddba950d108e014119b1cd6
Yakkala Yagnesh Raghava <hi@yagnesh.org>
2015-01-03 22:07:35 +0900

    * realClean.sh: test commit

--- realClean.sh
+++ realClean.sh
@@ -1,16 +1,6 @@
 #!/bin/bash
-#
-# Copyright (C) Yagnesh Raghava Yakkala. http://yagnesh.org
-#    File: realClean.sh
-# Created: Monday, September 19 2011
-# Licence: GPL v3 or later.
-#
-
-# Description:
 ## clean the products/garbage from real.sh
 
 rm -f real.log
 rm -f wrfinput_*
 rm -f wrfbdy_d*
-
-# realClean.sh ends here
